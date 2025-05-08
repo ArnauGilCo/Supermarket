1bd9d9eb6c8f71cbd8d7325048bc671d7a930630
Arnau Gil <a2409827@institutmontilivi.cat>
2025-05-08 18:17:54 +0200

    Update Customer.cs

--- Supermercat/Customer.cs
+++ Supermercat/Customer.cs
@@ -7,17 +7,33 @@ namespace Supermercat
 {
     public class Customer : Person
     {
+        //ATRIBUT
         private int? _fidelity_card;
+
+        /// <summary>
+        /// Constuctor que rep els parametres de Persona
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fullName"></param>
+        /// <param name="fidelityCard"></param>
         public Customer(string id, string fullName, int? fidelityCard) : base(id, fullName)
         {
             this._fidelity_card = fidelityCard;
         }
+        /// <summary>
+        /// LA propietat calcula el rating del costumer acord la quantitat de compres
+        /// El rating es el 2% de les compres
+        /// </summary>
+        /// <returns></returns>
         public override double GetRating()
         {
 
             return _totalInvoiced * 0.02;
         }
-
+        /// <summary>
+        /// Afegeix els punts
+        /// </summary>
+        /// <param name="point"></param>
         public override void AddPoints (int point)
         {
             _point = _point + point;
@@ -25,8 +41,7 @@ namespace Supermercat
 
         public override string ToString()
         {
-            return $"DNI/NIE-->{this._id} NOM-->{this._fullName}{base.ToString()}";
+            return $"DNI/NIE-->{this._id} NOM-->{this._fullName} rating  --> {this.GetRating} vendes -->{this._totalInvoiced}  punts -->{this._point} {base.ToString()}";
         }
-
     }
 }
