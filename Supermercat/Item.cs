46cabf67084ab4cfb952a6ad13ce7d8c00e6b188
ArnauGilCo <a2409827@institutmontilivi.cat>
2025-05-08 12:25:36 +0200

    Classe Supermercat + classe item

--- Supermercat/Item.cs
+++ Supermercat/Item.cs
@@ -14,7 +14,7 @@ namespace Supermercat
         public enum Packaging 
         { Unit, Kg, Package };
 
-        public const int nextCode = 1;
+        public static int nextCode = 1;
         
         private char currency = '\u20AC';
         private int code;
@@ -26,6 +26,18 @@ namespace Supermercat
         private double stock;
         private int minStock;
 
+        /// <summary>
+        /// Construcció d'un item a partir de tots els paràmetres.
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <param name="code"></param>
+        /// <param name="description"></param>
+        /// <param name="onSale"></param>
+        /// <param name="price"></param>
+        /// <param name="category"></param>
+        /// <param name="packaging"></param>
+        /// <param name="stock"></param>
+        /// <param name="minStock"></param>
         public Item(char currency, int code, string description, bool onSale, double price, Category category, Packaging packaging, double stock, int minStock)
         {
             this.currency = currency;
@@ -50,6 +62,9 @@ namespace Supermercat
 
         public bool OnSale {  get { return this.onSale; } }
 
+        /// <summary>
+        /// Retorna el preu tinguent en compte si el producte està o no està de rebaixes.
+        /// </summary>
         public double Price 
         { 
             get 
@@ -61,11 +76,20 @@ namespace Supermercat
             } 
         }
 
+        /// <summary>
+        /// Afegeix la qüantitat d'estoc passada com a paràmetre.
+        /// </summary>
+        /// <param name="item">item al que afegir-hi stock</param>
+        /// <param name="qty">qty a afegir al item</param>
         public static void UpdateStock(Item item, double qty)
         {
             item.stock += qty;
         }
 
+        /// <summary>
+        /// Genera un string amb l'informació necessària del item
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             string sale;
@@ -73,9 +97,30 @@ namespace Supermercat
             return $"CODE->{this.code} DESCRIPTION->{this.description}     CATEGORY->{this.GetCategory}    STOCK->{this.Stock} MIN_STOCK->{this.MinStock}  PRICE->{this.price}{currency} ONSALE->{sale}{currency}";
         }
 
+        /// <summary>
+        /// Compara 2 items a partir del seu stock.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public int CompareTo(Item? other)
         {
-            return this.code.CompareTo(other.code);
+            return this.stock.CompareTo(other.stock);
+        }
+
+        /// <summary>
+        /// Transforma un char a un valor de package.
+        /// </summary>
+        /// <param name="codiPack">char passat</param>
+        /// <returns></returns>
+        /// <exception cref="Exception">si no coincideix amb cap de les 3 opcions</exception>
+        public static Packaging charToPackage(char codiPack)
+        {
+            Packaging packaging;
+            if (codiPack == 'U') { packaging = Packaging.Unit; }
+            else if (codiPack == 'K') { packaging = Packaging.Kg; }
+            else if (codiPack == 'P') { packaging = Packaging.Package; }
+            else throw new Exception("Format incorrecte");
+            return packaging;
         }
     }
 }
