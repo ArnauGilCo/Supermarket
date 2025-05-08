e1f38240a0a2310a3ee0a5bf5a9b978e99588653
Arnau Gil <a2409827@institutmontilivi.cat>
2025-05-08 18:16:15 +0200

    Update Cashier.cs

--- Supermercat/Cashier.cs
+++ Supermercat/Cashier.cs
@@ -7,5 +7,60 @@ namespace Supermercat
 {
     public class Cashier : Person
     {
+        //Atributs
+        private DateTime _joiningDate;
+        
+        //Propietats
+        
+        /// <summary>
+        /// Hem de calcular els dias que porta de contratació
+        /// Per obtenir el rating fem una multiplicacio de daysOfService * (totalventes * 10%)
+        /// </summary>
+        /// <returns></returns>
+        public override double GetRating()
+        {
+            int daysOfService;
+            daysOfService = (DateTime.Now - _joiningDate).Days;
+            return daysOfService * (_totalInvoiced * 0.10);
+        }
+        
+        /// <summary>
+        /// Calcula els anys de servei de Cashier fent un caclul de la data de contratació - la data actual
+        /// </summary>
+        public int YearsOfService
+        {
+            get
+            {
+                return DateTime.Now.Year - _joiningDate.Year;
+            }
+        }
+        
+        //Constructor
+        /// <summary>
+        /// Constuctor que rep els parametres de Persona
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fullName"></param>
+        /// <param name="joiningDate"></param>
+        public Cashier(string id, string fullName, DateTime joiningDate) : base(id, fullName)
+        {
+            this._joiningDate = joiningDate;
+        }
+        
+        //METODES
+        /// <summary>
+        /// Afegeix els punts depenen els anys de servei
+        /// Es calcula fent AnysDeServei +1 i multiplicant-lo per pointsToAdd
+        /// </summary>
+        /// <param name="pointsToAdd"></param>
+        public override void AddPoints(int pointsToAdd)
+        {
+            _point += (YearsOfService + 1) * pointsToAdd;
+        }
+        
+        public override string ToString()
+        {
+            return $"DNI/NIE-->{this._id} NOM-->{this._fullName} rating  --> {this.GetRating} antiguitat -->{this.YearsOfService} vendes -->{this._totalInvoiced} punts -->{this._point} {base.ToString()}";
+        }
     }
 }
